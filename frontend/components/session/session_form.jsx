ef5834dac7bc0309d6c020a9888de4f94dd64060
alancln <alan.ng8912@gmail.com>
2022-06-21 18:24:37 -0400

    Change class components to functional components with hooks

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,99 +1,221 @@
-import React from 'react';
+import React, { useState } from 'react';
 import sleep from '../../util/sleep';
 
-class SessionForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.closeModal = this.closeModal.bind(this);
-        this.handleDemoUser = this.handleDemoUser.bind(this);
-    }
+// class SessionForms extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             username: '',
+//             password: ''
+//         }
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.closeModal = this.closeModal.bind(this);
+//         this.handleDemoUser = this.handleDemoUser.bind(this);
+//     }
 
-    handleSubmit(e) {
+//     handleSubmit(e) {
+//         e.preventDefault();
+//         this.props.processForm(this.state);
+//     }
+
+//     closeModal(e) {
+//         e.preventDefault();
+//         this.props.closeModal();
+//     }
+
+//     async handleDemoUser(e) {
+//         e.preventDefault();
+//         this.setState({username: 'demoUser', password: 'demouser'})
+//         await sleep(1200)
+//         this.props.login(this.state).then(() => this.props.closeModal());
+//     }
+
+//     update(field) {
+//         return e => this.setState({ [field]: e.target.value })
+//     }
+
+//     render() {
+
+//         let text = this.props.formType === 'login'
+//         ? text = 'Log in'
+//         : text = 'Continue'
+
+//         return (
+//             <div className="session-form-container">
+//                 <span className="modal-close" onClick={this.closeModal}>&times;</span>
+//                 <h1>Welcome to PixelPins</h1>
+//                 <div className="error-message-container">
+//                     {
+//                         this.props.errors.map((error, idx) => (
+//                             <p className="error-message" key={idx}>{error}</p>
+//                         ))
+//                     }
+//                 </div>
+//                 <form className="form-box">
+//                     <label htmlFor="username">
+//                         <div>Username</div>
+//                     </label>
+//                     <input
+//                         id="username"
+//                         type="text"
+//                         value={this.state.username}
+//                         placeholder="Username"
+//                         onChange={this.update('username')}
+//                     />
+//                     <label htmlFor="password">
+//                         <div>Password</div>
+//                     </label>
+//                     <input
+//                         id="password"
+//                         type="password"
+//                         value={this.state.password}
+//                         placeholder="Password"
+//                         onChange={this.update('password')}
+//                     />
+//                 </form>
+//                 <button 
+//                     className="form-submit-button"
+//                     onClick={this.handleSubmit}>
+//                         {text}
+//                 </button>
+//                 <span>OR</span>
+//                 <button
+//                     className="demo-user-button"
+//                     onClick={this.handleDemoUser}>
+//                     Continue as Demo User
+//                 </button>
+//                 <div className="filler-text-container">
+//                     <p>This clone is for educational purposes only.</p>
+//                     <p>Please do not put any sensitive information.</p>
+//                 </div>
+//                 <div className="line-divide"></div>
+//                 {this.props.otherForm}
+//             </div>
+//         )
+//     }
+// }
+
+
+const SessionForm = (props) => {
+
+    const [state, setState] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.props.processForm(this.state);
-        if (!this.props.errors) this.props.closeModal();
+        props.processForm(state)
     }
 
-    closeModal(e) {
+    const closeModal = (e) => {
         e.preventDefault();
-        this.props.closeModal();
+        props.closeModal();
     }
 
-    async handleDemoUser(e) {
+    async function handleDemoUser(e) {
         e.preventDefault();
-        this.setState({username: 'demoUser', password: 'demouser'})
-        await sleep(1200)
-        this.props.login(this.state).then(this.props.closeModal());
+        const demoUser = {username: 'demoUser', password: 'demouser'}
+        // setState({ ...state, username: 'demoUser', password: 'demouser'});
+        setState(Object.assign(state, demoUser))
+        await sleep(1200);
+        props.login(state).then(() => props.closeModal());
     }
 
-    update(field) {
-        return e => this.setState({ [field]: e.target.value })
+    const update = (field) => {
+        return e => setState({ ...state, [field]: e.target.value })
     }
 
-    render() {
 
-        let text = this.props.formType === 'login'
-        ? text = 'Log in'
-        : text = 'Continue'
+    let text = props.formType === 'login'
+    ? text = 'Log in'
+    : text = 'Continue'
 
-        return (
-            <div className="session-form-container">
-                <span className="modal-close" onClick={this.closeModal}>&times;</span>
-                <h1>Welcome to PixelPins</h1>
-                <div className="error-message-container">
-                    {
-                        this.props.errors.map((error, idx) => (
-                            <p className="error-message" key={idx}>{error}</p>
-                        ))
-                    }
-                </div>
-                <form className="form-box">
-                    <label htmlFor="username">
-                        <div>Username</div>
-                    </label>
-                    <input
-                        id="username"
-                        type="text"
-                        value={this.state.username}
-                        placeholder="Username"
-                        onChange={this.update('username')}
-                    />
-                    <label htmlFor="password">
-                        <div>Password</div>
-                    </label>
-                    <input
-                        id="password"
-                        type="password"
-                        value={this.state.password}
-                        placeholder="Password"
-                        onChange={this.update('password')}
-                    />
-                </form>
-                <button 
-                    className="form-submit-button"
-                    onClick={this.handleSubmit}>
-                        {text}
-                </button>
-                <span>OR</span>
-                <button
-                    className="demo-user-button"
-                    onClick={this.handleDemoUser}>
-                    Continue as Demo User
-                </button>
-                <div className="filler-text-container">
-                    <p>This clone is for educational purposes only.</p>
-                    <p>Please do not put any sensitive information.</p>
-                </div>
-                <div className="line-divide"></div>
-                {this.props.otherForm}
+    return (
+        <div className="session-form-container">
+            <span className="modal-close" onClick={closeModal}>&times;</span>
+            <h1>Welcome to PixelPins</h1>
+            <div className="error-message-container">
+                {
+                    props.errors.map((error, idx) => (
+                        <p className="error-message" key={idx}>{error}</p>
+                    ))
+                }
             </div>
-        )
-    }
+            <form className="form-box">
+                <label htmlFor="username">
+                    <div>Username</div>
+                </label>
+                <input
+                    id="username"
+                    type="text"
+                    value={state.username}
+                    placeholder="Username"
+                    onChange={update('username')}
+                />
+                <label htmlFor="password">
+                    <div>Password</div>
+                </label>
+                <input
+                    id="password"
+                    type="password"
+                    value={state.password}
+                    placeholder="Password"
+                    onChange={update('password')}
+                />
+            </form>
+            <button 
+                className="form-submit-button"
+                onClick={handleSubmit}>
+                    {text}
+            </button>
+            <span>OR</span>
+            <button
+                className="demo-user-button"
+                onClick={handleDemoUser}>
+                Continue as Demo User
+            </button>
+            <div className="filler-text-container">
+                <p>This clone is for educational purposes only.</p>
+                <p>Please do not put any sensitive information.</p>
+            </div>
+            <div className="line-divide"></div>
+            {props.otherForm}
+        </div>
+    )
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default SessionForm;
--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -1,55 +1,96 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Avatar from './avatar';
 
-class UserShow extends React.Component {
-    constructor(props) {
-        super(props);
+// class UserShow extends React.Component {
+//     constructor(props) {
+//         super(props);
 
-        this.followButton = this.followButton.bind(this);
-    }
+//         this.followButton = this.followButton.bind(this);
+//     }
 
-    componentDidMount() {
-        this.props.fetchUser(this.props.match.params.userId)
-    }
+//     componentDidMount() {
+//         this.props.fetchUser(this.props.match.params.userId)
+//     }
 
-    followButton() {
-        (this.props.user.id !== this.props.currentUser.id)
+//     componentDidUpdate() {
+//         this.props.fetchUser(this.props.match.params.userId)
+//     }
+
+//     followButton() {
+//         (this.props.user.id !== this.props.currentUser.id)
+//         ? <button className="follow-button">Follow</button>
+//         : null
+//     }
+
+//     render() {
+//         const { user } = this.props
+
+//         if (!user) return null;
+
+//         return ( 
+//             <div className="user-show-page">
+//                 <div className="user-show-info">
+//                     <Avatar
+//                         user={user}
+//                     />
+//                     <div className="username">
+//                         <h1 className="username-header">{user.username}</h1>
+//                         <span className="username-handle">{`@${user.username}`}</span>
+//                     </div>
+//                     <div className="follows-container">
+//                         <span>1 following</span>
+//                         <div className="follow-divider"></div>
+//                         <span>1 followers</span>
+//                     </div>
+//                     <div className="follow-button-container">
+//                         {this.followButton()}
+//                     </div>
+//                 </div>
+
+//             </div>
+//         );
+//     }
+// }
+
+const UserShow = (props) => {
+
+    const userParamsId = props.match.params.userId
+
+    useEffect(() => {
+        props.fetchUser(props.match.params.userId)
+    }, [userParamsId]);
+
+    const followButton = () => {
+        return (props.user.id !== props.currentUser.id)
         ? <button className="follow-button">Follow</button>
         : null
     }
 
-    render() { 
+    const { user } = props
 
-        const { user, currentUser } = this.props
+    if (!user) return null;
 
-        if (!user) return null;
-
-        return ( 
-            <div className="user-show-page">
-                <div className="user-show-info">
-                    <Avatar
-                        user={user}
-                    />
-                    {/* <div className="profile-pic-container">
-                        <div>Profile Pic Placeholder</div>
-                    </div> */}
-                    <div className="username">
-                        <h1 className="username-header">{user.username}</h1>
-                        <span className="username-handle">{`@${user.username}`}</span>
-                    </div>
-                    <div className="follows-container">
-                        <span>1 following</span>
-                        <div className="follow-divider"></div>
-                        <span>1 followers</span>
-                    </div>
-                    <div className="follow-button-container">
-                        {this.followButton()}
-                    </div>
+    return ( 
+        <div className="user-show-page">
+            <div className="user-show-info">
+                <Avatar
+                    user={user}
+                />
+                <div className="username">
+                    <h1 className="username-header">{user.username}</h1>
+                    <span className="username-handle">{`@${user.username}`}</span>
                 </div>
-
+                <div className="follows-container">
+                    <span>1 following</span>
+                    <div className="follow-divider"></div>
+                    <span>1 followers</span>
+                </div>
+                <div className="follow-button-container">
+                    {followButton()}
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
  
 export default UserShow;
