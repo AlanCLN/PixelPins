da82c023d3c94a9756272602b1caccefc7711bef
alancln <alan.ng8912@gmail.com>
2022-06-22 00:27:33 -0400

    Create error reducers for pins and boards

---
+++ frontend/reducers/board_errors_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_BOARD_ERRORS } from "../actions/board_actions";
+
+const _nullErrors = [];
+
+const boardErrorsReducer = (state = _nullErrors, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_BOARD_ERRORS:
+            return action.errors
+        default:
+            return state;
+    }
+}
+
+export default boardErrorsReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
+import boardErrorsReducer from "./board_errors_reducer";
+import pinErrorsReducer from "./pin_errors_reducer";
 import sessionErrorsReducer from "./session_errors_reducer";
 
 const errorsReducer = combineReducers({
-    session: sessionErrorsReducer
+    session: sessionErrorsReducer,
+    pins: pinErrorsReducer,
+    boards: boardErrorsReducer,
 })
 
 export default errorsReducer;
