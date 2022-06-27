4ab33ad04e7e1fd1f4e707b8e176f01aff3b1f77
alancln <alan.ng8912@gmail.com>
2022-06-26 21:53:39 -0400

    Have nice splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -13,34 +13,196 @@ const SplashPage = () => {
                 </div>
             </div>
             <div className="splash-page-pics">
-                <div className="column1">
-
+                <div className="splash-columns" id="column1">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood1.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood2.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination1.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination2.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby1.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby2.jpg" />
+                        </div>
+                    </div>
                 </div>
 
-                <div className="column2">
-                    
+                <div className="splash-columns" id="column2">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood3.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood4.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination3.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination4.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby3.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby4.jpg" />
+                        </div>
+                    </div>
                 </div>
 
-                <div className="column3">
-
+                <div className="splash-columns" id="column3">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood5.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood6.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination5.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination6.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby5.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby6.jpg" />
+                        </div>
+                    </div>
                 </div>
 
-                <div className="column4">
-
+                <div className="splash-columns" id="column4">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood7.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood8.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination7.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination8.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby7.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby8.jpg" />
+                        </div>
+                    </div>
                 </div>
 
-                <div className="column5">
-
+                <div className="splash-columns" id="column5">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood9.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood10.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination9.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination10.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby9.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby10.jpg" />
+                        </div>
+                    </div>
                 </div>
 
-                <div className="column6">
-
+                <div className="splash-columns" id="column6">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood11.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood12.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination11.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination12.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby11.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby12.jpg" />
+                        </div>
+                    </div>
                 </div>
-                
-                <div className="column7">
 
+                <div className="splash-columns" id="column7">
+                    <div className="splash-pics fave-dish">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood13.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashfood14.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics destinate">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination13.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashdestination14.jpg" />
+                        </div>
+                    </div>
+                    <div className="splash-pics hobbies">
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby13.jpg" />
+                        </div>
+                        <div className="splash-pin">
+                            <img src="https://pixel-pins-seeds.s3.amazonaws.com/splashhobby14.jpg" />
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div className="splash-gradient"></div>
         </div>
     )
 }
